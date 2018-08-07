9beac14ce6405ef7c15a65180e2ec5489da9263a
Justin Luoma <justin@justinluoma.com>
2018-08-07 09:17:27 -0500

    JavaScript with HTML: completed exercise 3-1.

--- js/external.js
+++ js/external.js
@@ -2,8 +2,20 @@
 
 console.log("Hello from external javascript!");
 
-alert("Welcome to my Website!");
+// alert("Welcome to my Website!");
+//
+// var color = prompt("What's your favorite color?");
+//
+// alert(color + " is your favorite color!");
 
-var color = prompt("What's your favorite color?");
+var little_mermaid = prompt("How long are you keeping The Little Mermaid for?");
 
-alert(color + " is your favorite color!");
+var brother_bear = prompt("How long are you keeping The Borther Bear for?");
+
+var hercules = prompt("How long are you keeping The Hercules for?");
+
+var cost = prompt("How does do movies cost per day?");
+
+var total = (Number(little_mermaid) + Number(brother_bear) + Number(hercules)) * Number(cost);
+
+alert("Your total with be: " + total);
