0935229b547e57c480adbaab91a705c6fdfb4266
Justin Luoma <justin@justinluoma.com>
2018-08-07 08:32:32 -0500

    JavaScript with HTML: completed exercises 1 and 2.

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Hello from external javascript!");
+
+alert("Welcome to my Website!");
+
+var color = prompt("What's your favorite color?");
+
+alert(color + " is your favorite color!");
